89ac42c591ad4ea17c187c2ffcd38993eacefd28
Priyanka Sarkar <Priyanka_Sarkar@epam.com>
2019-05-06 20:18:10 +0530

    This class defines the structure of a Binary Tree Node.

---
+++ src/com/implementation/binarytree/Node.java
@@ -0,0 +1,39 @@
+package com.implementation.binarytree;
+
+public class Node {
+	
+	int data;
+	Node leftChild;
+	Node rightChild;
+	
+	public Node(int data) {
+		this.data = data;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(Node leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public Node getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(Node rightChild) {
+		this.rightChild = rightChild;
+	}
+	
+}
